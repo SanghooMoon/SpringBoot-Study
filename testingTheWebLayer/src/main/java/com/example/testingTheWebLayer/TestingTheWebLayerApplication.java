e928f8f59b79b49bf09414e01519779d18394f3d
SanghooMoon <sanghoo.moon@gmail.com>
2021-10-20 22:41:20 +0900

    Add : Testing the Web Layer guide project

---
+++ testingTheWebLayer/src/main/java/com/example/testingTheWebLayer/TestingTheWebLayerApplication.java
@@ -0,0 +1,14 @@
+package com.example.testingTheWebLayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingTheWebLayerApplication {
+
+	public static void main(String[] args) throws Exception {
+		throw new Exception();
+		// SpringApplication.run(TestingTheWebLayerApplication.class, args);
+	}
+
+}
---
+++ testingTheWebLayer/src/test/java/com/example/testingTheWebLayer/TestingTheWebLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingTheWebLayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingTheWebLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
